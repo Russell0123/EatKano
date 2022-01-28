c528fe8165c98072cdd42a41bef57e458251504e
Russell0123 <98583617+Russell0123@users.noreply.github.com>
2022-01-28 18:50:56 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -178,7 +178,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
         _gameTimeNum--;
         _gameStartTime++;
         if (mode === MODE_NORMAL && _gameTimeNum <= 0) {
-            GameTimeLayer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;时间到！';
+            GameTimeLayer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;時間到了，家威射了';
             gameOver();
             GameLayerBG.className += ' flash';
             createjs.Sound.play("end");
@@ -193,7 +193,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             }
         } else if (mode === MODE_ENDLESS) {
             let cps = getCPS();
-            let text = (cps === 0 ? '计算中' : cps.toFixed(2));
+            let text = (cps === 0 ? '計算中' : cps.toFixed(2));
             GameTimeLayer.innerHTML = `CPS:${text}`;
         } else {
             GameTimeLayer.innerHTML = `SCORE:${_gameScore}`;
@@ -441,9 +441,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '加油啦，我明天要出國欸!';
-        if (cps <= 8) return '有點屌';
-        if (cps <= 10)  return '我認可你，臭甲!';
+        if (cps <= 3) return '加油啦，我明天要出國欸!';
+        if (cps <= 6) return '有點屌';
+        if (cps <= 9)  return '我認可你，臭甲!';
         if (cps <= 15) return '能這麼猛一定是班導';
         return '拜託別約談我Plz';
     }
